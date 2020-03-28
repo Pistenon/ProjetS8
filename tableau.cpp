4371ca80dc40a556242d14bf96e4693befb95162
pistenon <nicolas.pistenon@ecl18.ec-lyon.fr>
2020-03-28 18:08:06 +0100

    Correction de up : 	- Si le fait de monter n'a fait aucun changement dans la grille : on ne rajoute pas de case aléatoire 	- On ne remonte pas des 0, ça ne sert à rien
    Ajout de down
    
    Tests :
    	- sur up : pas de bug détecté
    	- sur down : pas de bug détecté

--- tableau.cpp
+++ tableau.cpp
@@ -84,29 +84,100 @@ void Tableau::Print()
 
 void Tableau::up()
 {
+    bool Changement=false ; // Permet de savoir si un seul changement a été fait dans la grille
     for (int i=1;i<taille;i++)
     {
         for (int j=0;j<taille;j++)
         {
-            bool test = true; // Booléen permettant de savoir si on a rencontré une case pleine au-dessus ou non, dès que c'est le cas, la boucle while s'arrête
-            int UP = 0;
-            while (test && i-(UP+1)>=0) //test si on a rencontré une case non vide, et si on peut encore remonter d'un cran dans le tableau
+            if (tab[i][j]!=0) // On ne remonte pas un 0
             {
-                if (tab[i-(UP+1)][j]==0)
+                bool test = true; // Booléen permettant de savoir si on a rencontré une case pleine au-dessus ou non, dès que c'est le cas, la boucle while s'arrête
+                int UP = 0;
+                while (test && i-(UP+1)>=0) //test si on a rencontré une case non vide, et si on peut encore remonter d'un cran dans le tableau
                 {
-                    UP+=1; //la case est non vide, on peut donc au moins remonter la cas jusque là
+                    if (tab[i-(UP+1)][j]==0)
+                    {
+                        UP+=1; //la case est non vide, on peut donc au moins remonter la cas jusque là
+                    }
+                    else
+                    {
+                        if (tab[i-(UP+1)][j]==tab[i][j])
+                        {
+                            UP+=1;
+                        }
+                        test=false; //On a rencontré une case non vide, donc on sort de la boucle
+                    }
                 }
-                else
+                if (UP !=0)
                 {
-                    test=false; //On a rencontré une case non vide, donc on sort de la boucle
+                    Changement=true;
+                    if (tab[i-UP][j]==tab[i][j])
+                    {
+                        tab[i-UP][j]=2*tab[i-UP][j];
+                        tab[i][j]=0;
+                    }
+                    else
+                    {
+                        tab[i-UP][j]=tab[i][j];
+                        tab[i][j]=0;
+                    }
+
                 }
-            }
-            if (UP !=0)
-            {
-                tab[i-UP][j]=tab[i][j];
-                tab[i][j]=0;
             }
         }
     }
-    CaseAleatoire();
+    if (Changement) // Si la grille n'a pas changé suite au mouvement, on ne rajoute pas de case aléatoire
+    {
+        CaseAleatoire();
+    }
+}
+
+void Tableau::down()
+{
+    bool Changement=false ; // Permet de savoir si un seul changement a été fait dans la grille
+    for (int i=taille-2;i>-1;i--)
+    {
+        for (int j=0;j<taille;j++)
+        {
+            if (tab[i][j]!=0) // On ne descend pas un 0
+            {
+                bool test = true; // Booléen permettant de savoir si on a rencontré une case pleine au-dessous ou non, dès que c'est le cas, la boucle while s'arrête
+                int DOWN = 0;
+                while (test && i+(DOWN+1)<taille) //test si on a rencontré une case non vide, et si on peut encore descendre d'un cran dans le tableau
+                {
+                    if (tab[i+(DOWN+1)][j]==0)
+                    {
+                        DOWN+=1; //la case est non vide, on peut donc au moins descendre la cas jusque là
+                    }
+                    else
+                    {
+                        if (tab[i+(DOWN+1)][j]==tab[i][j])
+                        {
+                            DOWN+=1;
+                        }
+                        test=false; //On a rencontré une case non vide, donc on sort de la boucle
+                    }
+                }
+                if (DOWN !=0)
+                {
+                    Changement=true;
+                    if (tab[i+DOWN][j]==tab[i][j])
+                    {
+                        tab[i+DOWN][j]=2*tab[i+DOWN][j];
+                        tab[i][j]=0;
+                    }
+                    else
+                    {
+                        tab[i+DOWN][j]=tab[i][j];
+                        tab[i][j]=0;
+                    }
+
+                }
+            }
+        }
+    }
+    if (Changement) // Si la grille n'a pas changé suite au mouvement, on ne rajoute pas de case aléatoire
+    {
+        CaseAleatoire();
+    }
 }
