4177742dea9027575a22453dba75e54b042af8aa
Lyspa <celine.raille@hotmail.fr>
2020-04-17 21:28:20 +0200

    ajout fonctions pour les deplacements

--- jeu.cpp
+++ jeu.cpp
@@ -2,6 +2,8 @@
 #include <random>
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -14,8 +16,8 @@ Jeu::Jeu(int tailletableau,QObject *parent) : QObject(parent)
 
     Init();
 
-    CaseAleatoire();
-    CaseAleatoire();
+    caseAleatoire();
+    caseAleatoire();
 
 }
 
@@ -52,7 +54,7 @@ void Jeu::Free()
 }
 
 
-void Jeu::CaseAleatoire()
+void Jeu::caseAleatoire()
 {
     // On suppose dans cette fonction qu'il y a au moins une case de libre dans le tableau (le test a été effectué avant l'appel de cette fonction)
     int Nombre1 = NbreAlea();
@@ -84,10 +86,510 @@ void Jeu::nouvellePartie()
 {
     Free();
     Init();
-    CaseAleatoire();
-    CaseAleatoire();
+    caseAleatoire();
+    caseAleatoire();
     Score=2;
 }
+
+bool Jeu::Cherche0()
+{
+    for (int i=0;i<taille;i++)
+    {
+        for (int j=0;j<taille;j++)
+        {
+            if (tab[i][j]==0)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+vector<vector<int>> Jeu::up()
+{
+    bool Changement=false ; // Permet de savoir si un seul changement a été fait dans la grille
+    vector<vector<int>> CHANGEMENT; // Permet de collecter tout les changements sur la grille pour l'affichage graphique
+    for (int i=1;i<taille;i++)
+    {
+        for (int j=0;j<taille;j++)
+        {
+            if (tab[i][j]!=0) // On ne remonte pas un 0
+            {
+                bool test = true; // Booléen permettant de savoir si on a rencontré une case pleine au-dessus ou non, dès que c'est le cas, la boucle while s'arrête
+                int UP = 0;
+                while (test && i-(UP+1)>=0) //test si on a rencontré une case non vide, et si on peut encore remonter d'un cran dans le tableau
+                {
+                    if (tab[i-(UP+1)][j]==0)
+                    {
+                        UP+=1; //la case est non vide, on peut donc au moins remonter la cas jusque là
+                    }
+                    else
+                    {
+                        if (tab[i-(UP+1)][j]==tab[i][j])
+                        {
+                            UP+=1;
+                        }
+                        test=false; //On a rencontré une case non vide, donc on sort de la boucle
+                    }
+                }
+                if (UP !=0)
+                {
+                    Changement=true;
+                    if (tab[i-UP][j]==tab[i][j])
+                    {
+                        tab[i-UP][j]=2*tab[i-UP][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i-UP;
+                        CHANGEMENT[s-1][3]=j;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j;
+                        CHANGEMENT[s-1][4]=tab[i-UP][j];
+                        if (tab[i-UP][j]>Score) //Seul moment où le score peut augmenter
+                        {
+                            Score=tab[i-UP][j];
+                        }
+                    }
+                    else
+                    {
+                        tab[i-UP][j]=tab[i][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i-UP;
+                        CHANGEMENT[s-1][3]=j;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j;
+                        CHANGEMENT[s-1][4]=tab[i-UP][j];
+                    }
+
+                }
+            }
+        }
+    }
+    if (Changement) // Si la grille n'a pas changé suite au mouvement, on ne rajoute pas de case aléatoire
+    {
+        if (Cherche0())
+        {
+            CHANGEMENT.push_back(vector<int>(1,1));
+        }
+        else
+        {
+            CHANGEMENT.push_back(vector<int>(1,0)); // la partie est finie, donc je l'indique par un 0 à la fin pour la gestion graphique
+        }
+    }
+    else
+        {
+            if (Cherche0())
+            {
+                CHANGEMENT.push_back(vector<int>(1,1)); //la partie peut continuer car il reste un 0
+            }
+            else {
+                CHANGEMENT.push_back(vector<int>(1,0)); //il ne reste plus de 0 et l'utilisateur a fait un mauvais coups, donc la partie se finit
+            }
+        }
+    if (Score>MeilleurScore)
+    {
+        MeilleurScore=Score;
+    }
+    return CHANGEMENT;
+}
+
+
+
+vector<vector<int>> Jeu::down()
+{
+    bool Changement=false ; // Permet de savoir si un seul changement a été fait dans la grille
+    vector<vector<int>> CHANGEMENT; // Permet de collecter tout les changements sur la grille pour l'affichage graphique
+    for (int i=taille-2;i>-1;i--)
+    {
+        for (int j=0;j<taille;j++)
+        {
+            if (tab[i][j]!=0) // On ne descend pas un 0
+            {
+                bool test = true; // Booléen permettant de savoir si on a rencontré une case pleine au-dessous ou non, dès que c'est le cas, la boucle while s'arrête
+                int DOWN = 0;
+                while (test && i+(DOWN+1)<taille) //test si on a rencontré une case non vide, et si on peut encore descendre d'un cran dans le tableau
+                {
+                    if (tab[i+(DOWN+1)][j]==0)
+                    {
+                        DOWN+=1; //la case est non vide, on peut donc au moins descendre la cas jusque là
+                    }
+                    else
+                    {
+                        if (tab[i+(DOWN+1)][j]==tab[i][j])
+                        {
+                            DOWN+=1;
+                        }
+                        test=false; //On a rencontré une case non vide, donc on sort de la boucle
+                    }
+                }
+                if (DOWN !=0)
+                {
+                    Changement=true;
+                    if (tab[i+DOWN][j]==tab[i][j])
+                    {
+                        tab[i+DOWN][j]=2*tab[i+DOWN][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i+DOWN;
+                        CHANGEMENT[s-1][3]=j;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j;
+                        CHANGEMENT[s-1][4]=tab[i+DOWN][j];
+                        if (tab[i+DOWN][j]>Score) //Seul moment où le score peut augmenter
+                        {
+                            Score=tab[i+DOWN][j];
+                        }
+                    }
+                    else
+                    {
+                        tab[i+DOWN][j]=tab[i][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i+DOWN;
+                        CHANGEMENT[s-1][3]=j;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j;
+                        CHANGEMENT[s-1][4]=tab[i+DOWN][j];
+                    }
+
+                }
+            }
+        }
+    }
+    if (Changement) // Si la grille n'a pas changé suite au mouvement, on ne rajoute pas de case aléatoire
+    {
+        if (Cherche0())
+        {
+            CHANGEMENT.push_back(vector<int>(1,1));
+        }
+        else
+        {
+            CHANGEMENT.push_back(vector<int>(1,0)); // la partie est finie, donc je l'indique par un 0 à la fin pour la gestion graphique
+        }
+    }
+    else
+        {
+            if (Cherche0())
+            {
+                CHANGEMENT.push_back(vector<int>(1,1)); //la partie peut continuer car il reste un 0
+            }
+            else {
+                CHANGEMENT.push_back(vector<int>(1,0)); //il ne reste plus de 0 et l'utilisateur a fait un mauvais coups, donc la partie se finit
+            }
+        }
+    if (Score>MeilleurScore)
+    {
+        MeilleurScore=Score;
+    }
+    return CHANGEMENT;
+}
+
+vector<vector<int>> Jeu::left()
+{
+    bool Changement=false ; // Permet de savoir si un seul changement a été fait dans la grille
+    vector<vector<int>> CHANGEMENT; // Permet de collecter tout les changements sur la grille pour l'affichage graphique
+    for (int j=taille-2;j<taille;j++)
+    {
+        for (int i=0;i<taille;i++)
+        {
+            if (tab[i][j]!=0) // On ne décalle pas un 0
+            {
+                bool test = true; // Booléen permettant de savoir si on a rencontré une case pleine à gauche ou non, dès que c'est le cas, la boucle while s'arrête
+                int LEFT = 0;
+                while (test && j-(LEFT+1)>-1)
+                {
+                    if (tab[i][j-(LEFT+1)]==0)
+                    {
+                        LEFT+=1;
+                    }
+                    else
+                    {
+                        if (tab[i][j-(LEFT+1)]==tab[i][j])
+                        {
+                            LEFT+=1;
+                        }
+                        test=false; //On a rencontré une case non vide, donc on sort de la boucle
+                    }
+                }
+                if (LEFT !=0)
+                {
+                    Changement=true;
+                    if (tab[i][j-LEFT]==tab[i][j])
+                    {
+                        tab[i][j-LEFT]=2*tab[i][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i;
+                        CHANGEMENT[s-1][3]=j-LEFT;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j;
+                        CHANGEMENT[s-1][4]=tab[i][j-LEFT];
+                        if (tab[i][j-LEFT]>Score) //Seul moment où le score peut augmenter
+                        {
+                            Score=tab[i][j-LEFT];
+                        }
+                    }
+                    else
+                    {
+                        tab[i][j-LEFT]=tab[i][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i;
+                        CHANGEMENT[s-1][3]=j-LEFT;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j;
+                        CHANGEMENT[s-1][4]=tab[i][j-LEFT];
+                    }
+
+                }
+            }
+        }
+    }
+    if (Changement) // Si la grille n'a pas changé suite au mouvement, on ne rajoute pas de case aléatoire
+    {
+        if (Cherche0())
+        {
+            CHANGEMENT.push_back(vector<int>(1,1));
+        }
+        else
+        {
+            CHANGEMENT.push_back(vector<int>(1,0)); // la partie est finie, donc je l'indique par un 0 à la fin pour la gestion graphique
+        }
+    }
+    else
+        {
+            if (Cherche0())
+            {
+                CHANGEMENT.push_back(vector<int>(1,1)); //la partie peut continuer car il reste un 0
+            }
+            else {
+                CHANGEMENT.push_back(vector<int>(1,0)); //il ne reste plus de 0 et l'utilisateur a fait un mauvais coups, donc la partie se finit
+            }
+        }
+    if (Score>MeilleurScore)
+    {
+        MeilleurScore=Score;
+    }
+    return CHANGEMENT;
+}
+
+vector<vector<int>> Jeu::right()
+{
+    bool Changement=false ; // Permet de savoir si un seul changement a été fait dans la grille
+    vector<vector<int>> CHANGEMENT; // Permet de collecter tout les changements sur la grille pour l'affichage graphique
+    for (int j=1;j>-1;j--)
+    {
+        for (int i=0;i<taille;i++)
+        {
+            if (tab[i][j]!=0) // On ne décalle pas un 0
+            {
+                bool test = true;
+                int RIGHT = 0;
+                while (test && j+(RIGHT+1)>-1)
+                {
+                    if (tab[i][j+(RIGHT+1)]==0)
+                    {
+                        RIGHT+=1;
+                    }
+                    else
+                    {
+                        if (tab[i][j+(RIGHT+1)]==tab[i][j])
+                        {
+                            RIGHT+=1;
+                        }
+                        test=false; //On a rencontré une case non vide, donc on sort de la boucle
+                    }
+                }
+                if (RIGHT !=0)
+                {
+                    Changement=true;
+                    if (tab[i][j+RIGHT]==tab[i][j])
+                    {
+                        tab[i][j+RIGHT]=2*tab[i][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i;
+                        CHANGEMENT[s-1][3]=j+RIGHT;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j+RIGHT;
+                        CHANGEMENT[s-1][4]=tab[i][j+RIGHT];
+                        if (tab[i][j+RIGHT]>Score) //Seul moment où le score peut augmenter
+                        {
+                            Score=tab[i][j+RIGHT];
+                        }
+                    }
+                    else
+                    {
+                        tab[i][j+RIGHT]=tab[i][j];
+                        tab[i][j]=0;
+                        CHANGEMENT.push_back(vector<int>(5));
+                        int s=CHANGEMENT.size();
+                        CHANGEMENT[s-1][2]=i;
+                        CHANGEMENT[s-1][3]=j+RIGHT;
+                        CHANGEMENT[s-1][0]=i;
+                        CHANGEMENT[s-1][1]=j+RIGHT;
+                        CHANGEMENT[s-1][4]=tab[i][j+RIGHT];
+                    }
+
+                }
+            }
+        }
+    }
+    if (Changement) // Si la grille n'a pas changé suite au mouvement, on ne rajoute pas de case aléatoire
+    {
+        if (Cherche0())
+        {
+            CHANGEMENT.push_back(vector<int>(1,1));
+        }
+        else
+        {
+            CHANGEMENT.push_back(vector<int>(1,0)); // la partie est finie, donc je l'indique par un 0 à la fin pour la gestion graphique
+        }
+    }
+    else
+        {
+            if (Cherche0())
+            {
+                CHANGEMENT.push_back(vector<int>(1,1)); //la partie peut continuer car il reste un 0
+            }
+            else {
+                CHANGEMENT.push_back(vector<int>(1,0)); //il ne reste plus de 0 et l'utilisateur a fait un mauvais coups, donc la partie se finit
+            }
+        }
+    if (Score>MeilleurScore)
+    {
+        MeilleurScore=Score;
+    }
+    return CHANGEMENT;
+}
+
+
+
+
+void Jeu::move_up()
+{
+    vector<vector<int>> CHANGEMENT = up();
+    if (CHANGEMENT[0].size()>1)
+    {
+        int N = CHANGEMENT.size();
+        for (int i=0;i<N-1;i++)
+        {
+            int a = CHANGEMENT[i][0];
+            int b = CHANGEMENT[i][1];
+            int an = CHANGEMENT[i][2];
+            int bn = CHANGEMENT[i][3];
+            int val = CHANGEMENT[i][4];
+            tab[a][b] = 0;
+            tab[an][bn] = val;
+        }
+        if (CHANGEMENT[N-1][0] == 1)
+        {
+            caseAleatoire();
+        }
+    }
+    caseChanged();
+}
+
+void Jeu::move_down()
+{
+    vector<vector<int>> CHANGEMENT = down();
+    if (CHANGEMENT[0].size()>1)
+    {
+        int N = CHANGEMENT.size();
+        for (int i=0;i<N-1;i++)
+        {
+            int a = CHANGEMENT[i][0];
+            int b = CHANGEMENT[i][1];
+            int an = CHANGEMENT[i][2];
+            int bn = CHANGEMENT[i][3];
+            int val = CHANGEMENT[i][4];
+            tab[a][b] = 0;
+            tab[an][bn] = val;
+        }
+        if (CHANGEMENT[N-1][0] == 1)
+        {
+            caseAleatoire();
+        }
+    }
+    caseChanged();
+}
+
+void Jeu::move_left()
+{
+    vector<vector<int>> CHANGEMENT = left();
+    if (CHANGEMENT[0].size()>1)
+    {
+        int N = CHANGEMENT.size();
+        for (int i=0;i<N-1;i++)
+        {
+            int a = CHANGEMENT[i][0];
+            int b = CHANGEMENT[i][1];
+            int an = CHANGEMENT[i][2];
+            int bn = CHANGEMENT[i][3];
+            int val = CHANGEMENT[i][4];
+            tab[a][b] = 0;
+            tab[an][bn] = val;
+        }
+        if (CHANGEMENT[N-1][0] == 1)
+        {
+            caseAleatoire();
+        }
+    }
+    caseChanged();
+}
+
+void Jeu::move_right()
+{
+    vector<vector<int>> CHANGEMENT = right();
+    if (CHANGEMENT[0].size()>1)
+    {
+        int N = CHANGEMENT.size();
+        for (int i=0;i<N-1;i++)
+        {
+            int a = CHANGEMENT[i][0];
+            int b = CHANGEMENT[i][1];
+            int an = CHANGEMENT[i][2];
+            int bn = CHANGEMENT[i][3];
+            int val = CHANGEMENT[i][4];
+            tab[a][b] = 0;
+            tab[an][bn] = val;
+        }
+        if (CHANGEMENT[N-1][0] == 1)
+        {
+            caseAleatoire();
+        }
+    }
+    caseChanged();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 int Jeu::get_case(int x,int y)
